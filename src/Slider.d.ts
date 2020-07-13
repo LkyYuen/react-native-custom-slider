237518092bdcea015890f1945875b845b6e18ae8
khaiyuen <khaiyuen.lau@zchwantech.com>
2020-07-13 10:41:32 +0800

    add declaration file

---
+++ src/Slider.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for react-native-custom-slider 1.0.0
+// Project: https://github.com/LkyYuen/react-native-custom-slider
+
+declare module 'react-native-custom-slider' {
+
+    const Slider: {
+        slider: () => void
+    };
+
+    export = Slider;
+}
